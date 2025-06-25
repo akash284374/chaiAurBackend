20a35f2fd47046bcec1cb4f2a93a805eee7eebb8
Akash <akash284374@gmail.com>
2025-06-25 17:14:49 +0530

    2nd commit

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const express=require('express')
 const app=express()
 const Port=4000;
 
+const githubData={
+    "message": "Not Found",
+    "documentation_url": "https://docs.github.com/rest",
+    "status": "404"
+}
+
 app.get('/',(req,res)=>{
     res.send("Hello World!");
+})
+
+app.get('/gitHub',(req,res)=>{
+    res.json(githubData);
 })
 
 app.get('/login',(req,res)=>{
